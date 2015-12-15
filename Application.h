cb34738bb69fab5eab67299a4c7cfc3685004c96
MetroGoldyMayer <amphiros@gmail.com>
2015-12-15 12:48:49 +0100

    Starting Commit
    Base game

---
+++ Application.h
@@ -0,0 +1,42 @@
+#ifndef __APPLICATION_CPP__
+#define __APPLICATION_CPP__
+
+#include<list>
+#include "Globals.h"
+#include "Module.h"
+
+class ModuleRender;
+class ModuleWindow;
+class ModuleTextures;
+class ModuleInput;
+class ModuleAudio;
+class ModuleFadeToBlack;
+
+class Application
+{
+public:
+
+	Application();
+	~Application();
+
+	bool Init();
+	update_status Update();
+	bool CleanUp();
+
+public:
+	ModuleRender* renderer;
+	ModuleWindow* window;
+	ModuleTextures* textures;
+	ModuleInput* input;
+	ModuleAudio* audio;
+	ModuleFadeToBlack* fade;
+
+private:
+
+	std::list<Module*> modules;
+
+};
+
+extern Application* App;
+
+#endif // __APPLICATION_CPP__
